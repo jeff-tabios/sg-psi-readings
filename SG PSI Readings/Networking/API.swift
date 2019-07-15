77922625284bc686b7d977552438a9675aa4e6c6
jeff-tabios <jefftabios1@gmail.com>
2019-07-16 02:06:08 +0800

    Refactored API

--- SG PSI Readings/Networking/API.swift
+++ SG PSI Readings/Networking/API.swift
@@ -11,42 +11,25 @@ import Networking
 
 class API{
     
-    func mockRequest(_ filename: String, completion: (Reading?)-> Void) {
-        if let path = Bundle.main.path(forResource: "sample", ofType: "json") {
-            do {
-                let fileUrl = URL(fileURLWithPath: path)
-                
-                let data = try Data(contentsOf: fileUrl, options: .mappedIfSafe)
-                
-                let decoder = JSONDecoder()
-                decoder.dateDecodingStrategy = .iso8601
-                
-                guard let reading = try? decoder.decode(Reading.self, from: data) else{
-                    print("Error decoding data")
-                    completion(nil)
-                    
-                    return
-                }
-                
-                completion(reading)
-                
-            } catch {
-                completion(nil)
-            }
-        }
-    }
-    
-    func liveRequest(dateTime: String? = nil,
-                     date:String? = nil,
-                     completion: @escaping (Reading?,ReadingError?)-> Void){
+    func createParems(date:String?=nil) -> [String:String]{
         
         var params:[String:String] = [:]
         
-        if dateTime != nil{
-            params["date_time"] = dateTime
-        }else if  date != nil{
-            params["date"] = date
+        if let date = date {
+            if date.count == 10{
+                params["date"] = date
+            }else{
+                params["date_time"] = date
+            }
         }
+        
+        return params
+    }
+    
+    func liveRequest(date:String? = nil,
+                     completion: @escaping (Reading?,ReadingError?)-> Void){
+        
+        let params = createParems(date: date)
         
         let networking = Networking(baseURL: NetworkConfig.baseUrl)
         networking.get(NetworkConfig.psiUrl,parameters: params) { (result) in
@@ -80,4 +63,31 @@ class API{
             
         }
     }
+    
+    
+    func mockRequest(_ filename: String, completion: (Reading?)-> Void) {
+        if let path = Bundle.main.path(forResource: "sample", ofType: "json") {
+            do {
+                let fileUrl = URL(fileURLWithPath: path)
+                
+                let data = try Data(contentsOf: fileUrl, options: .mappedIfSafe)
+                
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                
+                guard let reading = try? decoder.decode(Reading.self, from: data) else{
+                    print("Error decoding data")
+                    completion(nil)
+                    
+                    return
+                }
+                
+                completion(reading)
+                
+            } catch {
+                completion(nil)
+            }
+        }
+    }
+    
 }
