0a7ee19b62b186c99569d42f88adcb173b2f7b04
jeff-tabios <jefftabios1@gmail.com>
2019-07-16 16:26:56 +0800

    Added helper to allow access to property via subscript

---
+++ SG PSI Readings/Helpers/PropertyReflectable.swift
@@ -0,0 +1,21 @@
+//
+//  PropertyReflectable.swift
+//  SG PSI Readings
+//
+//  Created by Jeff Tabios on 16/07/2019.
+//  Copyright © 2019 Jeff Tabios. All rights reserved.
+//
+
+import Foundation
+
+protocol PropertyReflectable { }
+
+extension PropertyReflectable {
+    subscript(key: String) -> Any? {
+        let m = Mirror(reflecting: self)
+        return m.children.first { $0.label == key }?.value
+    }
+}
+
+
+extension ReadingValue: PropertyReflectable {}
