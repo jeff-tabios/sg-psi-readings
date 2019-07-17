a8d67af09a357f85ffd1c66c40b2ebababd0d6b7
jeff-tabios <jefftabios1@gmail.com>
2019-07-18 00:45:01 +0800

    Updated date helper

--- SG PSI Readings/Helpers/DateExtension.swift
+++ SG PSI Readings/Helpers/DateExtension.swift
@@ -11,8 +11,10 @@ import Foundation
 extension Date{
     func toString()->String{
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-ddTHH:mm:ss"
-        return formatter.string(from: self)
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        formatter.timeZone = TimeZone(abbreviation: "SGT")
+        let d = formatter.string(from: self)
+        return d.replacingOccurrences(of: " ", with: "T")
     }
     func toStr()->String{
         let formatter = DateFormatter()
